570773769b8cae73999ef5cf0c928d0d3a9f97d4
Joshua Bailey <joshbaileydev513@gmail.com>
2023-04-12 01:22:04 -0400

    created get route for retrieving all of the saved notes and a post route for new notes to save

--- Develop/server.js
+++ Develop/server.js
@@ -17,6 +17,36 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for index.html file
-app.get('*', (req, res) =>
+app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for retrieving all the saved notes
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for tips`);
+    readFromFile('./db/.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+// POST Route for a new note to save
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a new note`);
+  
+    const { note_id, title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        note_id: uuid(),
+        title: req.body.title,
+        text: req.body.text,
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully!`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });  
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
